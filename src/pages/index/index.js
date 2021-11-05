0540c9538b7edab6ebc1418ff0bd58a07b367cbc
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-11-05 11:48:32 +0700

    chore: init project

---
+++ src/pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+  list() {
+    my.navigateTo({ url: 'pages/list/index' })
+  },
+  grid() {
+    my.navigateTo({ url: 'pages/grid/index' })
+  },
+  footer() {
+    my.navigateTo({ url: 'pages/footer/index' })
+  },
+  scroll() {
+    my.navigateTo({ url: 'pages/scroll/index' })
+  },
+  placeholder() {
+    my.navigateTo({ url: 'pages/placeholder/index' })
+  },
+});
